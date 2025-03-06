3e665be64a322487a415a6f4ad8b80c2ecc0dd60
edalca <edwinalonso162@hotmail.com>
2025-03-06 00:41:30 -0600

    cambios logo

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -7,7 +7,7 @@ app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+app_logo_url = "/assets/erpnext/images/sodex-logo.svg"
 
 
 add_to_apps_screen = [
@@ -110,8 +110,8 @@ calendars = ["Task", "Work Order", "Sales Order", "Holiday List", "ToDo"]
 website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
-	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
-	"splash_image": "/assets/erpnext/images/erpnext-logo.svg",
+	"favicon": "/assets/erpnext/images/sodex-favicon.icon",
+	"splash_image": "/assets/erpnext/images/sodex-logo.svg",
 }
 
 # nosemgrep
@@ -479,7 +479,7 @@ scheduler_events = {
 	],
 }
 
-email_brand_image = "assets/erpnext/images/erpnext-logo.jpg"
+email_brand_image = "assets/erpnext/images/sodex-logo.jpg"
 
 default_mail_footer = """
 	<span>

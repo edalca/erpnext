9f5efc14af3b131654264170bb3ebb7113434da6
edalca <edwinalonso162@hotmail.com>
2025-03-22 12:22:41 -0600

    cambio company

--- erpnext/setup/doctype/company/company.py
+++ erpnext/setup/doctype/company/company.py
@@ -29,7 +29,6 @@ class Company(Document):
 		abbr: DF.Data
 		accumulated_depreciation_account: DF.Link | None
 		allow_account_creation_against_child_company: DF.Check
-		amended_from: DF.Link | None
 		asset_received_but_not_billed: DF.Link | None
 		auto_err_frequency: DF.Literal["Daily", "Weekly", "Monthly"]
 		auto_exchange_rate_revaluation: DF.Check
